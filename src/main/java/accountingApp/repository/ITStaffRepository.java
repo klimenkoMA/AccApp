dd8d91d16d941174aaa239862328cf79e1a1dd80
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-07-22 21:18:15 +0300

    Refactored some entity fields in itStaff classes v2

--- src/main/java/accountingApp/repository/ITStaffRepository.java
+++ src/main/java/accountingApp/repository/ITStaffRepository.java
@@ -2,18 +2,18 @@ package accountingApp.repository;
 
 import accountingApp.entity.ITStaff;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ITStaffRepository extends JpaRepository<ITStaff, Integer> {
-//	@Query("SELECT c FROM ITStaff c")
+	@Override
 	List<ITStaff> findAll();
 
-	List<ITStaff> findPersonsProfById(int id);
+//	List<ITStaff> findPersonsProfById(int id);
 
-	void deleteById(int id);
+	@Override
+	void deleteById(Integer id);
 
 }
