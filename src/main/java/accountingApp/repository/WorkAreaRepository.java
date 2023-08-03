98051df820e5b2e6f4c89f845bbf934621cf357f
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-08-03 20:32:59 +0300

    Repaired findById method in WorkArea Classes

--- src/main/java/accountingApp/repository/WorkAreaRepository.java
+++ src/main/java/accountingApp/repository/WorkAreaRepository.java
@@ -12,7 +12,7 @@ public interface WorkAreaRepository extends JpaRepository<WorkArea, Integer> {
 	@Override
 	List<WorkArea> findAll();
 
-//	List<WorkArea> findBycAeger(int id);
+	List<WorkArea> findAreaById(int id);
 
 	@Override
 	void deleteById(Integer integer);
