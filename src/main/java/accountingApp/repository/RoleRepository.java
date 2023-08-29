126aa8cee69b3b1950a64a118944feb5fde2f634
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-08-29 19:42:28 +0300

    Deleted some files

--- src/main/java/accountingApp/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-/* package accountingApp.repository;
-
-import accountingApp.security.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
- */
