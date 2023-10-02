0ef0c7ac1bee0412fc58a8ffa480bcbcc7c87a2c
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-10-02 20:40:29 +0300

    Rebuilt js package

--- src/main/java/accountingApp/AccountingApplication.java
+++ src/main/java/accountingApp/AccountingApplication.java
@@ -2,13 +2,28 @@ package accountingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Main class
  */
 @SpringBootApplication
-public class AccountingApplication {
+public class AccountingApplication implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/",
+            "classpath:/static/js"
+    };
+
     public static void main(String[] args) {
         SpringApplication.run(AccountingApplication.class, args);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
 }
