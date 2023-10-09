7609023596605d4d4006f846c24ec1ea6f5f54bc
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-10-09 06:18:06 +0300

    Tried again to make a popup in devices.html

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,39 +1,44 @@
-// document.getElementById("demo")
-    // .innerHTML = "The text was changed by JavaScript!";
+onclick = function () {
 
-button.onclick = function() {
-    clickCount(++iClick, baseInfo);
+    // clickCount(++iClick, baseInfo);
 
     let mainWidth = document.documentElement.offsetWidth;
     let mainHeight = document.documentElement.offsetHeight;
-    let params = `width=500,height=400,left=${mainWidth/2-500/2},top=${mainHeight/2-400/2}`;
-    let popup = window.open('http://192.168.0.101/main', 'Popup', params);
+    let params = `width=500,height=400,left=${mainWidth / 2 - 500 / 2},top=${mainHeight / 2 - 400 / 2}`;
+    let popup = window.open('http://192.168.0.101/main.html', 'Popup', params);
 
     popup.focus();
 
-    popup.addEventListener('DOMContentLoaded', function() {
-        let x = 1,
-            y = 1;
+    // popup.addEventListener('DOMContentLoaded', function() {
+    //     let x = 1,
+    //         y = 1;
+    //
+    //     setInterval(() => {
+    //         if (popup.screenX >= window.screen.availWidth - popup.outerWidth) x = -x;
+    //         if (popup.screenX <= 0) x = -x;
+    //         if (popup.screenY >= window.screen.availHeight - popup.outerHeight) y = -y;
+    //         if (popup.screenY <= 0) y = -y;
+    //
+    //         popup.moveBy(x, y);
+    //     }, 1);
+    //
+    //     setTimeout(() => {
+    //         if (!popup.closed) popup.close()
+    //         else console.log('The pop-up window was closed manually.');
+    //     }, 60e3);
+    // });
+    //
+    // document.addEventListener('mousedown', popupFocus);
+    // document.addEventListener('keydown', popupFocus);
+    //
+    // function popupFocus() {
+    //     popup.focus();
+    // }
+};
 
-        setInterval(() => {
-            if (popup.screenX >= window.screen.availWidth - popup.outerWidth) x = -x;
-            if (popup.screenX <= 0) x = -x;
-            if (popup.screenY >= window.screen.availHeight - popup.outerHeight) y = -y;
-            if (popup.screenY <= 0) y = -y;
+click = function () {
+    let params = `width=500,height=400,left=500/2,top=400/2`;
+    let popup = window.open('http://192.168.0.101/main.html', 'Popup', params);
 
-            popup.moveBy(x, y);
-        }, 1);
-
-        setTimeout(() => {
-            if (!popup.closed) popup.close()
-            else console.log('The pop-up window was closed manually.');
-        }, 60e3);
-    });
-
-    document.addEventListener('mousedown', popupFocus);
-    document.addEventListener('keydown', popupFocus);
-
-    function popupFocus() {
-        popup.focus();
-    }
+    popup.focus();
 };
