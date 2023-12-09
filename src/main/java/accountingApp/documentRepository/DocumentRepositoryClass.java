54cc5dd47dcc164470ddb9e5f1d83f6414f2bcd0
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-12-09 15:48:59 +0300

    Tech commit

--- src/main/java/accountingApp/documentRepository/DocumentRepositoryClass.java
+++ src/main/java/accountingApp/documentRepository/DocumentRepositoryClass.java
@@ -2,8 +2,11 @@ package accountingApp.documentRepository;
 
 import accountingApp.documentEntity.DocumentClass;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface DocumentRepositoryClass extends MongoRepository <DocumentClass, String> {
 
+    @Query ("{'name' : {$regex: ?0, $options: 'i' }}")
+    DocumentClass findByName (String name);
 
 }
--- src/main/java/accountingApp/documentService/DocumentServiceClass.java
+++ src/main/java/accountingApp/documentService/DocumentServiceClass.java
@@ -1,4 +1,17 @@
 package accountingApp.documentService;
 
+
+import accountingApp.documentEntity.DocumentClass;
+import accountingApp.documentRepository.DocumentRepositoryClass;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DocumentServiceClass {
+
+    @Autowired
+    DocumentRepositoryClass documentRepositoryClass;
+
+    public void addDocument(DocumentClass doc){
+
+    }
 }
+
