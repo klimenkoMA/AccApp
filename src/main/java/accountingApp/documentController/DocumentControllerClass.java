9d4e4871fbcf9b217bc7908bb433aac3368fcec4
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-12-13 21:06:18 +0300

    Created add method in DocumentControllerClass

--- src/main/java/accountingApp/documentController/DocumentControllerClass.java
+++ src/main/java/accountingApp/documentController/DocumentControllerClass.java
@@ -1,4 +1,14 @@
 package accountingApp.documentController;
 
+import accountingApp.documentService.DocumentServiceClass;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DocumentControllerClass {
+
+    @Autowired
+    DocumentServiceClass documentServiceClass;
+
+    public String addNewDocument(String doc){
+        return "page";
+    }
 }
