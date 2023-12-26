48cbafec4237176e5fc833d2025d5a55bbad4281
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-12-26 19:30:53 +0300

    Added comment in DocumentRepositoryClass.java

--- src/main/java/accountingApp/documentRepository/DocumentRepositoryClass.java
+++ src/main/java/accountingApp/documentRepository/DocumentRepositoryClass.java
@@ -4,6 +4,10 @@ import accountingApp.documentEntity.DocumentClass;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+/**
+ * Стандартный класс-интерфейс для связи с БД
+ */
+
 public interface DocumentRepositoryClass extends MongoRepository <DocumentClass, String> {
 
     @Query ("{'name' : {$regex: ?0, $options: 'i' }}")
