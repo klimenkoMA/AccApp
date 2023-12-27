553b5810457f90444831e6d886ef4d7d4d8937df
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-12-27 05:39:31 +0300

    Added comment in DocumentControllerClass.java

--- src/main/java/accountingApp/documentController/DocumentControllerClass.java
+++ src/main/java/accountingApp/documentController/DocumentControllerClass.java
@@ -3,6 +3,14 @@ package accountingApp.documentController;
         import accountingApp.documentService.DocumentServiceClass;
         import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Класс для взаимодействия со слоем VIEW, непосредственно HTML-страницами.
+ * Содержит в себе методы, которые на вход принимают вводимые пользователем
+ * данные либо нажатие на элементы дизайна страницы(кнопки, ссылки). В методе
+ * эти данные обрабатываються и пересылаются дальше по цепочке в класс Сервиса.
+ * Полученный оттуда ответ возвращается пользователю в виде изменений на HTML-странице
+ */
+
 public class DocumentControllerClass {
 
     @Autowired
