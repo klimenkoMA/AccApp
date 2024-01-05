c1c66ece20fb77380436faa13b394c757cae438c
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-01-05 14:49:38 +0300

    Added mongoDB connection to project

--- src/main/java/accountingApp/documentRepository/DocumentRepositoryClass.java
+++ src/main/java/accountingApp/documentRepository/DocumentRepositoryClass.java
@@ -1,18 +1,21 @@
-//package accountingApp.documentRepository;
-//
-//import accountingApp.documentEntity.DocumentClass;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.data.mongodb.repository.Query;
-//
-///**
-// * Стандартный класс-интерфейс для связи с БД
-// */
-//
-//public interface DocumentRepositoryClass extends MongoRepository <DocumentClass, String> {
-//
-//    @Query ("{'name' : {$regex: ?0, $options: 'i' }}")
-//    DocumentClass findByName (String name);
-//
+package accountingApp.documentRepository;
+
+import accountingApp.documentEntity.DocumentClass;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Стандартный класс-интерфейс для связи с БД
+ */
+@Repository
+@EnableMongoRepositories
+public interface DocumentRepositoryClass extends MongoRepository <DocumentClass, String> {
+
+    @Query ("{'name' : {$regex: ?0, $options: 'i' }}")
+    DocumentClass findByName (String name);
+
 //    DocumentClass findAll(String name);
-//
-//}
+
+}
