3da507dc043ee626bf503006def66abc08a90417
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-01-07 09:10:52 +0300

    Added findAll impl`s in some classes and created documents.html

--- src/main/java/accountingApp/documentService/DocumentServiceClass.java
+++ src/main/java/accountingApp/documentService/DocumentServiceClass.java
@@ -6,6 +6,8 @@ import accountingApp.documentRepository.DocumentRepositoryClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Класс для реализации основной логики методов CRUD.
  * Прослойка между репозиторием и контроллером.
@@ -16,6 +18,10 @@ public class DocumentServiceClass {
 
     @Autowired
     DocumentRepositoryClass documentRepositoryClass;
+
+    public List<DocumentClass> findAllDocuments(){
+        return documentRepositoryClass.findAll();
+    }
 
     public void addDocument(DocumentClass doc) {
 
