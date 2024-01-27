476494cb029dac27372ea9a66d2f80b6f2124aa7
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-01-27 15:53:17 +0300

    Solved some issues with css styles in login.html

--- src/main/java/accountingApp/config/WebSecurityConfig.java
+++ src/main/java/accountingApp/config/WebSecurityConfig.java
@@ -20,15 +20,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                   .antMatchers("/", "/resources/**","/static/**", "/css/**" ).permitAll()
+                   .anyRequest().authenticated()
+
+
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                    .logout()
+                    .permitAll();
     }
 
     @Bean
