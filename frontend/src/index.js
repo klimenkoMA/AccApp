2faa77a1b5cff399aeda38718c1dbe890ad0afcc
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-02-02 06:20:13 +0300

    Added new script in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,17 @@ root.render(
   </React.StrictMode>
 );
 
+const express = require('express');
+const app = express();
+
+app.get('/react', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
