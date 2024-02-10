de44941ce78b5ae4e33514491f6337c0634ea558
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-02-10 12:27:53 +0300

    Changed versions of node js and mongoose

--- frontend/src/training.js
+++ frontend/src/training.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import {registerValidation} from "./validations/auth.js";
+import {validationResult} from 'express-validator';
+import UserModel from "./models/User.js";
 
-mongoose.connect('mongodb://localhost:27018/upcdocs')
+mongoose.connect('mongodb://localhost:27017/reactdb')
+// mongoose.connect('localhost:27017/reactdb')
     .then(() => {
         console.log('DB connected')
             .catch(() => 'DB error', err);
     });
-
-console.log("bugaga");
 
 const app = express();
 
@@ -19,15 +21,31 @@ app.get('/', (req, res) => {
         'Вот томатик, и вот томатик!');
 });
 
-app.post('/auth/register', (req, res) => {
+app.post('/auth/register', registerValidation,async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
 
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
 
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        passwordHash,
+        avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
 });
+
+
 
 app.listen(4444, (err) => {
     if (err) {
         return console.log(err);
     }
-
     console.log('Server OK');
 });
---
+++ frontend/src/validations/auth.js
@@ -0,0 +1,12 @@
+import {body} from 'express-validator';
+
+
+export const registerValidation = [
+    body('email', 'Wrong email format').isEmail(),
+    body('password', 'Password must be at least 5 characters').isLength({min: 5}),
+    body('fullName',"Please enter name").isLength({min: 3}),
+    body('avatarUrl', 'Wrong avatar link').optional().isURL(),
+
+];
+
+
