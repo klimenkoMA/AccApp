19b4600dedbe86969ba2f369c070499453ed93d8
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-02-14 06:05:56 +0300

    Rebuilt some js files

--- frontend/src/training.js
+++ frontend/src/training.js
@@ -1,10 +1,43 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import * as mongoose from "mongoose";
+import {registerValidation} from './validations/auth.js';
+import {validationResult} from "express-validator";
+import UserModel from './models/User.js';
+
+
+mongoose.connect('mongodb://localhost:27017/nodedb')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello client');
+app.use(express.json());
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
+
 
 app.listen(4444, (err) => {
     if (err) {
