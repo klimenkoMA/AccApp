9b00dde64e57f6f6ba4b6a2ab68cfe661c876f18
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-02-18 08:24:54 +0300

    Added some new js files and corrected training.js

--- frontend/src/utils/checkAuth.js
+++ frontend/src/utils/checkAuth.js
@@ -2,8 +2,24 @@ import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
 
-        const token = req.headers.authorization;
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+    // const token = req.headers.authorization || '';
 
-        res.send(token);
-
+    console.log(token);
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, 'secret123');
+            req.userId = decoded._id;
+            next();
+        } catch (err) {
+            console.log(err);
+            return res.status(403).json({
+                message: 'No access no access'
+            });
+        }
+    } else {
+        return res.status(403).json({
+            message: 'No access'
+        });
+    }
 }
