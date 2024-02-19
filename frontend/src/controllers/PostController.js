d1e351e70086e637daa4a8fb7d5d53acaeded455
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-02-19 06:02:39 +0300

    Added Post.js

--- frontend/src/controllers/PostController.js
+++ frontend/src/controllers/PostController.js
@@ -1,7 +1,24 @@
-export const create = async (req, res) =>{
-    try{
-        
-    }catch (err) {
-        
+import PostModel from '../models/Post.js';
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            imageURL: req.body.imageURL,
+            tags: req.body.tags,
+            user: req.userId,
+        });
+
+        const post = await doc.save();
+
+        res.json(post);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Could not create an article',
+        });
+
     }
 }
