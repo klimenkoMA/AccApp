51076c01d9da0f1292a0a49d566d6b9d1ac769ef
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-02-21 05:24:13 +0300

    Added some methods in training.js to check REST in Insomnia

--- frontend/src/training.js
+++ frontend/src/training.js
@@ -4,6 +4,8 @@ import {registerValidation, loginValidation} from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js'
 import * as PostController from './controllers/PostController.js'
+import jwt from "jsonwebtoken";
+
 
 
 mongoose.connect('mongodb://localhost:27017/nodedb')
@@ -14,6 +16,27 @@ const app = express();
 
 app.use(express.json());
 
+
+app.get('/', (req, res) =>{
+   console.log(req.body);
+
+   res.json({
+       success: true,
+   });
+});
+
+app.post('/test', (req, res) =>{
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Vasya Pupkin',
+    }, 'secret123',
+    );
+
+    res.json({
+        success: true,
+        token,
+    });
+});
 // app.get('/posts', PostController.getAll);
 // app.get('/posts/:id', PostController.getOne);
 app.post('/posts', PostController.create);
--- frontend/src/utils/checkAuth.js
+++ frontend/src/utils/checkAuth.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken';
 export default (req, res, next) => {
 
     const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
-    // const token = req.headers.authorization || '';
 
     console.log(token);
     if (token) {
