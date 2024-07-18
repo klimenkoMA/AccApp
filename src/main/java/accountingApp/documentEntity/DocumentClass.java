6a987b2918782800b9d1b4c7cc103572ac476f40
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-07-18 20:54:00 +0300

    Some big changes closely connected with moving to another OS.

--- src/main/java/accountingApp/documentEntity/DocumentClass.java
+++ src/main/java/accountingApp/documentEntity/DocumentClass.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
@@ -12,6 +13,7 @@ import javax.persistence.Id;
  */
 
 @Document(collection = "documents")
+//@Entity
 public class DocumentClass {
 
     @Id
