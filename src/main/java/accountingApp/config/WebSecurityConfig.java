cf7664a7b308bc5a6d5e15df2fc9569ea35dbe9f
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-10-07 05:17:18 +0300

    Added new users in App

--- src/main/java/accountingApp/config/WebSecurityConfig.java
+++ src/main/java/accountingApp/config/WebSecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
@@ -45,13 +48,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("u")
-                        .password("1")
-                        .roles("USER")
-                        .build();
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder()
+                .username("u")
+                .password("1")
+                .roles("USER")
+                .build());
+        users.add(User.withDefaultPasswordEncoder()
+                .username("u2")
+                .password("1")
+                .roles("USER")
+                .build());
+        users.add(User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1")
+                .roles("ADMIN")
+                .build());
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(users);
     }
+
+
 }
