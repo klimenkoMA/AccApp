984289bbb3f783525c518d644d605dc2d4141ccb
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-10-12 11:04:33 +0300

    Built a couple of success tests v3

---
+++ src/test/java/accountingApp/controller/FeedbackControllerTest.java
@@ -0,0 +1,59 @@
+package accountingApp.controller;
+
+import accountingApp.entity.Feedback;
+import accountingApp.service.FeedbackService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.mockito.Mockito.*;
+
+class FeedbackControllerTest {
+
+    @InjectMocks
+    private FeedbackController feedbackController;
+
+    @Mock
+    private FeedbackService feedbackService;
+
+    @Mock
+    Model model;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getFeedbacks() {
+
+        Feedback f1 = new Feedback();
+        Feedback f2 = new Feedback();
+        Feedback f3 = new Feedback();
+
+        List<Feedback> feedbackList = Arrays.asList(f1, f2, f3);
+
+        Mockito.when(feedbackService.findAllFeedbacks()).thenReturn(feedbackList);
+
+        String viewName = feedbackController.getFeedbacks(model);
+
+        Assertions.assertEquals("feedbacks", viewName);
+
+        verify(model).addAttribute("feedbackList", feedbackList);
+
+        verify(feedbackService).findAllFeedbacks();
+    }
+
+    @Test
+    void addNewFeedback() {
+    }
+}
