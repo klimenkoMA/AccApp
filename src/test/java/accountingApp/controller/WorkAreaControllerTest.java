eb07e70a7b4d3ca5ac8a5f72da191e2efcca8def
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-10-18 20:13:37 +0300

    Added CRUD buttons into feedbacks.html and created some unit tests

--- src/test/java/accountingApp/controller/WorkAreaControllerTest.java
+++ src/test/java/accountingApp/controller/WorkAreaControllerTest.java
@@ -1,5 +1,6 @@
 package accountingApp.controller;
 
+import accountingApp.entity.ITStaff;
 import accountingApp.entity.WorkArea;
 import accountingApp.service.WorkAreaService;
 import org.junit.jupiter.api.Assertions;
@@ -103,14 +104,142 @@ class WorkAreaControllerTest {
     }
 
     @Test
-    void deleteWorkArea() {
+    void deleteWorkAreaValid() {
+        String id = "15";
+        int idCheck = Integer.parseInt(id);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.deleteWorkArea(id, model);
+
+        Assertions.assertEquals("workarea", viewName);
+
+        verify(model, times(2)).addAttribute("workAreaList", workAreaList);
+
+        verify(workAreaService, times(1)).deleteWorkAreaById(idCheck);
     }
 
     @Test
-    void updateWorkArea() {
+    void deleteWorkAreaFail() {
+        String id = "-15";
+        int idCheck = Integer.parseInt(id);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.deleteWorkArea(id, model);
+
+        Assertions.assertEquals("workarea", viewName);
+
+        verify(model).addAttribute("workAreaList", workAreaList);
+
+        verify(workAreaService, never()).deleteWorkAreaById(idCheck);
     }
 
     @Test
-    void findAreaByName() {
+    void deleteWorkAreaFailWithException() {
+        String id = "15";
+        int idCheck = Integer.parseInt(id);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        doThrow(new RuntimeException()).when(workAreaService).deleteWorkAreaById(idCheck);
+
+        verify(model, never()).addAttribute("workAreaList", workAreaList);
+
+        verify(workAreaService, never()).deleteWorkAreaById(idCheck);
+    }
+
+    @Test
+    void updateWorkAreaValid() {
+        String id = "15";
+        String workAreaTitle = "GTU";
+        int idCheck = Integer.parseInt(id);
+        WorkArea workArea = new WorkArea(idCheck, workAreaTitle);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.updateWorkArea(id, workAreaTitle, model);
+
+        Assertions.assertEquals("workarea", viewName);
+
+        verify(model, times(2)).addAttribute("workAreaList", workAreaList);
+
+        verify(workAreaService, times(1)).updateWorkArea(any());
+    }
+
+    @Test
+    void updateWorkAreaFail() {
+        String id = "-15";
+        String workAreaTitle = "GTU";
+        int idCheck = Integer.parseInt(id);
+
+        WorkArea workArea = new WorkArea(idCheck, workAreaTitle);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.updateWorkArea(id, workAreaTitle, model);
+
+        Assertions.assertEquals("workarea", viewName);
+
+        verify(model, times(1)).addAttribute("workAreaList", workAreaList);
+
+        verify(workAreaService, never()).updateWorkArea(workArea);
+    }
+
+    @Test
+    void updateWorkAreaFailWithException() {
+        String id = "15";
+        String workAreaTitle = "GTU";
+        int idCheck = Integer.parseInt(id);
+
+        WorkArea workArea = new WorkArea(idCheck, workAreaTitle);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.updateWorkArea(id, workAreaTitle, model);
+
+        doThrow(new RuntimeException()).when(workAreaService).updateWorkArea(workArea);
+
+        verify(workAreaService, never()).updateWorkArea(workArea);
+    }
+
+    @Test
+    void findWorkAreaValid() {
+        String workAreaTitle = "15";
+        int idCheck = Integer.parseInt(workAreaTitle);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.findAreaByName(workAreaTitle, model);
+
+        Assertions.assertEquals("workarea", viewName);
+
+        verify(workAreaService, times(1)).getWorkAreaById(idCheck);
+    }
+
+    @Test
+    void findWorkAreaFail() {
+        String workAreaTitle = "-15";
+        int idCheck = Integer.parseInt(workAreaTitle);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        String viewName = workAreaController.findAreaByName(workAreaTitle, model);
+
+        Assertions.assertEquals("workarea", viewName);
+
+        verify(workAreaService, never()).getWorkAreaById(idCheck);
+    }
+
+    @Test
+    void findWorkAreaFailWithException() {
+        String workAreaTitle = "15";
+        int idCheck = Integer.parseInt(workAreaTitle);
+
+        Mockito.when(workAreaService.findAllWorkArea()).thenReturn(workAreaList);
+
+        doThrow(new RuntimeException()).when(workAreaService).getWorkAreaById(idCheck);
+
+        verify(workAreaService, never()).getWorkAreaById(idCheck);
     }
 }
