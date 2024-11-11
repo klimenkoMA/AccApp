65a0e0b5536354cf5a9252bee499e510bbff65ad
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-11-11 05:56:40 +0300

    Created a CustomUserDetailService.java

---
+++ src/main/java/accountingApp/service/CustomUserDetailService.java
@@ -0,0 +1,41 @@
+package accountingApp.service;
+
+import accountingApp.entity.AppUser;
+import accountingApp.repository.AppUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+class CustomUserDetailsService implements UserDetailsService {
+
+    final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
+    @Autowired
+    private AppUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        try {
+            AppUser appUser = userRepository.findByUsername(username);
+            if (appUser == null) {
+                throw new UsernameNotFoundException("User not found");
+            }
+
+            // Создайте UserDetails на основе информации о пользователе
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(appUser.getUserName())
+                    .password(appUser.getUserPass())
+                    .roles(appUser.getRoles().split(",")) // предположим, что роли хранятся через запятую
+                    .build();
+        }catch (Exception e){
+            logger.error("User not found " + e.getMessage());
+            return null;
+        }
+    }
+}
