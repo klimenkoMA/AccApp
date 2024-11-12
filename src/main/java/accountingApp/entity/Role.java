bc59f48fe05be3f9086c674b08873475b9468040
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-11-12 06:03:43 +0300

    Created a Role.class

---
+++ src/main/java/accountingApp/entity/Role.java
@@ -0,0 +1,14 @@
+package accountingApp.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER,
+    ADMIN,
+    ;
+
+    @Override
+    public String getAuthority() {
+        return  name();
+    }
+}
