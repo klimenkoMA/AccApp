ad46d3654480f9d52773be5c5838c4a0828afa43
klimenkoMA <klimenko.ma.1982@gmail.com>
2024-11-15 21:43:00 +0300

    Rebuilt a AppUserService.java v2

--- src/main/java/accountingApp/service/CustomUserDetailService.java
+++ src/main/java/accountingApp/service/CustomUserDetailService.java
@@ -1,48 +1,48 @@
-package accountingApp.service;
-
-import accountingApp.entity.AppUser;
-import accountingApp.entity.Role;
-import accountingApp.repository.AppUserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-
-
-@Service
-class CustomUserDetailsService implements UserDetailsService {
-
-    final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
-
-    @Autowired
-    private AppUserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        try {
-            AppUser appUser = userRepository.findByUsername(username);
-            if (appUser == null) {
-                throw new UsernameNotFoundException("User not found");
-            }
-
-            // Создайте UserDetails на основе информации о пользователе
-
-            return User
-                    .withUsername(appUser.getUserName())
-                    .password(appUser.getUserPass())
-                    .roles(appUser.getRoles()
-                            .stream()
-                            .map(Role::getAuthority).toArray(String[]::new))
-                    .build();
-        }catch (Exception e){
-            logger.error("User not found with name: " + username + " " + e.getMessage());
-            throw new UsernameNotFoundException("User not found " + e.getMessage());
-        }
-    }
-}
+//package accountingApp.service;
+//
+//import accountingApp.entity.AppUser;
+//import accountingApp.entity.Role;
+//import accountingApp.repository.AppUserRepository;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//
+//
+//@Service
+//class CustomUserDetailsService implements UserDetailsService {
+//
+//    final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+//
+//    @Autowired
+//    private AppUserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//
+//        try {
+//            AppUser appUser = userRepository.findByUserName(userName).get(0);
+//            if (appUser == null) {
+//                throw new UsernameNotFoundException("User not found");
+//            }
+//
+//            // Создайте UserDetails на основе информации о пользователе
+//
+//            return User
+//                    .withUsername(appUser.getUserName())
+//                    .password(appUser.getUserPass())
+//                    .roles(appUser.getRoles()
+//                            .stream()
+//                            .map(Role::getAuthority).toArray(String[]::new))
+//                    .build();
+//        }catch (Exception e){
+//            logger.error("User not found with name: " + userName + " " + e.getMessage());
+//            throw new UsernameNotFoundException("User not found " + e.getMessage());
+//        }
+//    }
+//}
