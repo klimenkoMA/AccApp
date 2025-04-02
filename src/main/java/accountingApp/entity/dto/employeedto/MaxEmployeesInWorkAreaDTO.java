f7fef1c782abfb08a86681fd6767147c2173d1a5
klimenkoMA <klimenko.ma.1982@gmail.com>
2025-04-02 05:30:18 +0300

    Added new method reportingMaxEmployeesCountInWorkArea in EmployeeRepository

--- src/main/java/accountingApp/entity/dto/employeedto/MaxEmployeesInWorkAreaDTO.java
+++ src/main/java/accountingApp/entity/dto/employeedto/MaxEmployeesInWorkAreaDTO.java
@@ -1,4 +1,31 @@
 package accountingApp.entity.dto.employeedto;
 
 public class MaxEmployeesInWorkAreaDTO {
+
+    private int employeesCount;
+    private String workAreaName;
+
+    public MaxEmployeesInWorkAreaDTO() {
+    }
+
+    public MaxEmployeesInWorkAreaDTO(int employeesCount, String workAreaName) {
+        this.employeesCount = employeesCount;
+        this.workAreaName = workAreaName;
+    }
+
+    public int getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public void setEmployeesCount(int employeesCount) {
+        this.employeesCount = employeesCount;
+    }
+
+    public String getWorkAreaName() {
+        return workAreaName;
+    }
+
+    public void setWorkAreaName(String workAreaName) {
+        this.workAreaName = workAreaName;
+    }
 }
